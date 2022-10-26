96da79c17df1b0e2043111e535a1ca79f937314c
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-10-26 09:46:53 +0200

    Set up network deployment

--- hardhat-fund-me-fcc/deploy/01-deploy-fund-me.js
+++ hardhat-fund-me-fcc/deploy/01-deploy-fund-me.js
@@ -20,6 +20,7 @@ const {
   developmentChains,
 } = require("../helper-hardhat-config");
 const { network } = require("hardhat");
+const { verify } = require("../utils/verify");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
@@ -41,11 +42,20 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   // if the contract doesn't exist, we deploy a minimal version of it for local testing
 
   // when going for localhost or hardhat network we want to use a mock
+  const args = [ethUsdPriceFeedAddress];
   const fundMe = await deploy("FundMe", {
     from: deployer,
-    args: [ethUsdPriceFeedAddress], // put price feed address,
+    args: args, // put price feed address,
     log: true,
   });
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    // verify
+    await verify(fundMe.address, args);
+  }
   log("------------------------------");
 };
 
--- hardhat-fund-me-fcc/hardhat.config.js
+++ hardhat-fund-me-fcc/hardhat.config.js
@@ -1,9 +1,31 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-deploy");
 
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
+const GOERLI_RPC_URL =
+  process.env.GOERLI_RPC_URL ||
+  "https://eth-mainnet.alchemyapi.io/v2/your-api-key";
+const PRIVATE_KEY =
+  process.env.PRIVATE_KEY ||
+  "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   // solidity: "0.8.8",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      // gasPrice: 130000000000,
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+      blockConfirmations: 6,
+    },
+  },
   solidity: {
     compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
   },
