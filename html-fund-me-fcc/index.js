9018bb71de9f15c819203a83b7ac77819f1bd74e
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-10-27 10:07:42 +0200

    Add basic Metamask connection with vanilla JS

---
+++ html-fund-me-fcc/index.js
@@ -0,0 +1,23 @@
+const connectButton = document.getElementById("connectButton");
+// const withdrawButton = document.getElementById("withdrawButton");
+// const fundButton = document.getElementById("fundButton");
+// const balanceButton = document.getElementById("balanceButton");
+connectButton.onclick = connect;
+// withdrawButton.onclick = withdraw;
+// fundButton.onclick = fund;
+// balanceButton.onclick = getBalance;
+
+async function connect() {
+  if (typeof window.ethereum !== "undefined") {
+    try {
+      await ethereum.request({ method: "eth_requestAccounts" });
+    } catch (error) {
+      console.log(error);
+    }
+    connectButton.innerHTML = "Connected";
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    console.log(accounts);
+  } else {
+    connectButton.innerHTML = "Please install MetaMask";
+  }
+}
