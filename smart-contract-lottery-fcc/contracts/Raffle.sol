2496810477cf1baa47ca57af8f2355201cb973b1
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-11-02 10:40:01 +0100

    Implementing Chainlink VRF Request

--- smart-contract-lottery-fcc/contracts/Raffle.sol
+++ smart-contract-lottery-fcc/contracts/Raffle.sol
@@ -11,6 +11,7 @@
 pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Raffle__NotEnoughETHEntered();
 
@@ -20,14 +21,32 @@ contract Raffle is VRFConsumerBaseV2 {
     uint256 private immutable i_entranceFee;
     // Must be payable, because one of the players will win the lottery and we will have to pay them
     address payable[] private s_players;
+    // If a variable will be set up only once, it makes sense to make it private and immutable
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
 
     /* Events */
     event RaffleEnter(address indexed player);
+    event RequestedRaffWinner(uint256 indexed requestId);
 
     // We want the entrance fee to be configurable so we set it up in the constructor
     // VRFConsumerBaseV2 need to pass it, this is the address of the contract that does the random number verification
-    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -47,6 +66,14 @@ contract Raffle is VRFConsumerBaseV2 {
         // Request the random number
         // Once we get it, do something with it
         // 2 transaction process
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, //gasLane, maximum gas price you are willing to pay for a request in wei
+            i_subscriptionId, // the subscription id that this contract uses for funding requests
+            REQUEST_CONFIRMATIONS, // how many confirmations Chainlink node should wait before responding
+            i_callbackGasLimit, // the limit for how much gas to use for the callback request to your contract's fulfillRandomWords() function
+            NUM_WORDS // how many random numbers we want to get
+        );
+        emit RequestedRaffWinner(requestId);
     }
 
     // This function comes from chainlinks VRFConsumerBaseV2 contract, where it is marked as an virtual function, which means it expected to be overridden
