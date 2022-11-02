818cc1a9066d891d5f2a1c64290864bda091ab7c
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-11-02 10:15:03 +0100

    Add events, inherit chainlink contract

--- smart-contract-lottery-fcc/contracts/Raffle.sol
+++ smart-contract-lottery-fcc/contracts/Raffle.sol
@@ -10,17 +10,23 @@
 
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Raffle__NotEnoughETHEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     /* State Variable */
     // Make it immutable to save some gas
     uint256 private immutable i_entranceFee;
     // Must be payable, because one of the players will win the lottery and we will have to pay them
     address payable[] private s_players;
 
+    /* Events */
+    event RaffleEnter(address indexed player);
+
     // We want the entrance fee to be configurable so we set it up in the constructor
-    constructor(uint256 entranceFee) {
+    // VRFConsumerBaseV2 need to pass it, this is the address of the contract that does the random number verification
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
     }
 
@@ -32,9 +38,24 @@ contract Raffle {
         // Keep track of all players that enter the lottery
         s_players.push(payable(msg.sender));
         // Emit an event when we update a dynamic array or mapping
+        // Named events with the function name reversed
+        emit RaffleEnter(msg.sender);
     }
 
-    // function pickRandomWinner() {}
+    // External functions are a bit cheaper, because Solidity knows the contract can't call them, can only be called from the "outside"
+    function requestRandomWinner() external {
+        // Request the random number
+        // Once we get it, do something with it
+        // 2 transaction process
+    }
+
+    // This function comes from chainlinks VRFConsumerBaseV2 contract, where it is marked as an virtual function, which means it expected to be overridden
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
+
+    /* View / Pure functions */
 
     // Show entrance fee value
     function getEntranceFee() public view returns (uint256) {
